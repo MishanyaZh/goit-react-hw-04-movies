8b1ebc6614b07d714f4c0b912079bedf79b10f01
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-10-02 21:10:27 +0200

    onGoBack

--- src/components/views/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/views/MovieDetailsPage/MovieDetailsPage.jsx
@@ -37,6 +37,9 @@ export default function MovieDetailsPage() {
   }, [movieId]);
 
   const onGoBack = () => {
+    if (!location.state) {
+      return history.push('/');
+    }
     history.push(location.state.from);
   };
 
